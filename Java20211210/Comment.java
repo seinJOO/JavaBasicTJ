d87363642906059cfe0dec47c17e759781122e29
seinJOO <sein19950910@gmail.com>
2021-12-10 14:36:01 +0900

    virtual

---
+++ Java20211210/Comment.java
@@ -0,0 +1,15 @@
+
+
+// 클래스명 선언
+public class Comment { //코드의 우측에도 사용
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    // 주석 comment 
+    // 라인 주석
+    /*
+    블록 주석    
+    */ 
+    
+
+}
---
+++ Java20211210/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args) {
+        //자바에서는 변수 선언 시 반드시 자료형을 함께 선언해야만 한다
+        int a = 5;
+        String b = "Lunch";
+
+        System.out.println(a);
+        
+    }
+}
